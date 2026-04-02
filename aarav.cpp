eaa049af14cd9977991de1912114618772290a36
aarav sheiwastava <aaravshriwastava92@gmai.com>
2026-04-02 16:33:36 +0530

    new code

---
+++ aarav.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int col,row,n;
+
+  for(row=1;row<=5;row=row+1)
+   {
+    for(col=1;col<=5-row;col=col+1)
+    {
+        cout<<"  ";
+    }
+    for(col=1;col<=2*row-1;col=col+1)
+    {
+        cout<<"*"<<" ";
+    }
+    cout<<endl;
+   }
+}
+
+
